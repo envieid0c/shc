6cbb97a5b5438f8954edba5f971e0555f3f29744
envieid0c <envieidoc@gmail.com>
2016-07-09 16:32:21 +0300

    automake 1.14.1 -> 1.15

--- src/shc.c
+++ src/shc.c
@@ -17,11 +17,11 @@
  */
 
 static const char my_name[] = "shc";
-static const char version[] = "Version 3.9.3a1";
+static const char version[] = "Version 3.9.3a3";
 static const char subject[] = "Generic Shell Script Compiler";
 static const char cpright[] = "GNU GPL Version 3";
 static const struct { const char * f, * s, * e; }
-	provider = { "Jahidul", "Hamid", "<jahidulhamid@yahoo.com>" };          
+	provider = { "Fedor", "Mankov", "<envieidoc@gmail.com>" };
 
 /* 
 static const struct { const char * f, * s, * e; }
